f9c740e3e9aa73b539a730234def6702ecb3061c
imaustink <austin@austinkurpuis.com>
2018-03-24 15:36:28 -0700

    Added install command
    Closes imaustink/warhead#9, closes imaustink/warhead#10

--- bin/warhead.js
+++ bin/warhead.js
@@ -5,8 +5,26 @@ const yeoman = require('yeoman-environment')
 const cli = require('../lib')
 const deploy = require('../lib/deploy')
 const testRunner = require('../lib/test-runner')
+const installer = require('../lib/installer')
 const yeomanEnv = yeoman.createEnv()
 const program = new Command()
+const path = require('path')
+const fs = require('fs')
+function getAllServiceNames (promise) {
+  return new Promise((resolve, reject) => {
+    fs.readdir(path.join(process.cwd(), 'services'), (err, dirs) => {
+      if (err) {
+        return reject(err)
+      }
+      Promise.all(dirs.map(dir => {
+        const pathParts = dir.split(path.sep)
+        const name = pathParts.pop()
+        return promise(name)
+      })).then(resolve, reject)
+      resolve(dirs)
+    })
+  })
+}
 
-cli(process.argv, { yeomanEnv, testRunner, deploy, program })
+cli(process.argv, { yeomanEnv, testRunner, deploy, program, installer, getAllServiceNames })
   .then(result => result && console.log(result))
